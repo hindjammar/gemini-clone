38ae0812ed50fe6de803f4ad05572032494b4470
Hind_Jammar <121793980+hindjammar@users.noreply.github.com>
2024-12-31 20:34:17 +0100

    Save propmt in sidebar

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -19,17 +19,32 @@ const ContextProvider = (props) =>{
             setResultData(prev=>prev+nextWord);
           },75*index)
     }
+   
+
+    const newChat = () =>{
+        setLoading(false)
+        setShowResult(false)
+    }
+
 
     const onSent = async (prompt) =>{
 
        setResultData("")
        setLoading(true)
        setShowResult(true)
-       setRecentPrompt(input)
-       setPrevPrompts(prev=>[...prev,input])
-      const response =  await runChat(input)
+       let response;
+       if (prompt !== undefined) {
+           response = await runChat(prompt)
+           setRecentPrompt(prompt)
+       }
+       else{
+          setPrevPrompts(prev=>[...prev,input])
+          setRecentPrompt(input)
+          response = await runChat(input)
+       }
+       
       let responseArray =  response.split("**");
-      let newResponse;
+      let newResponse="";
       for (let i=0; i  <responseArray.length; i++)
       {
         if(i === 0 || i%2 !== 1){
@@ -66,6 +81,7 @@ const ContextProvider = (props) =>{
        resultData,
        input,
        setInput,
+       newChat
 
 
     }
